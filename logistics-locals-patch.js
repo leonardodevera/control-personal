ae4e2f8428626bae1b067ddaa37d75e2cd6f9d8a
leonardodevera <jleonardodeverag@gmail.com>
2026-09-04 18:48:13 -0500

    Deduplicate logistics store master list

--- logistics-locals-patch.js
+++ logistics-locals-patch.js
@@ -1,26 +1,20 @@
 (()=>{
-  const VERSION='excel-locals-v1';
+  const VERSION='excel-locals-v2';
   if(localStorage.getItem('cp-logistics-locals-version')===VERSION)return;
   if(typeof logisticsConfig==='undefined')return;
   const brand=logisticsConfig.brands.find(b=>b.name==='LA ITALIANA')||logisticsConfig.brands[0];
   if(!brand)return;
   const base={city:'GUAYAQUIL',brandId:brand.id,active:true,activities:{impulsacion:true,insumos:true,supervision:true,logistica:true},categories:{impulsadora:true,insumos:true,supervisor:true,logistica:true}};
   const names=[
-    'DISPROMARKS',
+    'DISPROMARK',
     'LYRYS VILLA CLUB',
-    'DISPROMARKS',
-    'LYRYS CIUDAD CELESTE',
-    'NIKAMAR ANTEPARA Y COLON',
-    'NIKAMAR 8 Y COLON',
-    'NIKAMAR 25 Y CALLEJON PARRA',
-    'NIKAMAR QUISQUIS',
-    'NIKAMAR QUISQUIS',
-    'LYRY DE PORTAL',
+    'LYRIS CIUDAD CELESTE',
     'NIKAMAR 9VENA Y COLON',
     'NIKAMAR 25 Y CALLEJON PARRA',
-    'NIKAMAR QUISQUIS'
+    'NIKAMAR QUISQUIS',
+    'LYRIS DE PORTAL'
   ];
-  logisticsConfig.stores=names.map((name,i)=>({id:'excel-'+(i+1),name,...JSON.parse(JSON.stringify(base))}));
+  logisticsConfig.stores=names.map((name,i)=>({id:'local-'+(i+1),name,...JSON.parse(JSON.stringify(base))}));
   saveLogistics();
   localStorage.setItem('cp-logistics-locals-version',VERSION);
   if(typeof renderLogistics==='function')renderLogistics();
